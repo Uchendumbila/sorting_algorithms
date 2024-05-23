fac9d43ce623ae470140702243490a41f01cf520
Uchendumbila <ekeuche1408@gmail.com>
2024-05-23 07:35:38 -0700

    Updates

--- sort.h
+++ sort.h
@@ -34,6 +34,7 @@ void counting_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void merge_sort(int *array, size_t size);
+void merge_recursion(int *arr, int *array, size_t left, size_t right);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
